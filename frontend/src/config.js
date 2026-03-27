2dfab7457c536749505d4bfd171a5e58c4532ce4
ninu <nishaad.dhabale@gmail.com>
2026-03-28 02:09:58 +0530

    right url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,1 @@
-///export const BACKEND_URL = 'https://swiftpay-1-ue1m.onrender.com';
-export const BACKEND_URL = 'http://localhost:3000';
+export const BACKEND_URL = 'https://swiftpay-1-ue1m.onrender.com';
